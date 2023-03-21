01941c0384e2ab017118dc87ea1dfd07244f1ea3
AlfredoSequeida <alfredosequeida@outlook.com>
2023-03-21 00:00:43 -0700

    updated server test

--- __tests__/server.test.mjs
+++ __tests__/server.test.mjs
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../app.mjs";
 
 describe("Test server connection", () => {
-  test("It should response the GET method", () => {
+  test("It should complete the get request", () => {
     return request(app).get("/").expect(200);
   });
 });
