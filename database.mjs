20d1d303b9f2e49495455a8f8f93c634da505f76
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-03-20 20:14:44 -0700

    ESLint Setup

--- database.mjs
+++ database.mjs
@@ -5,20 +5,20 @@ import "dotenv/config";
 const client = new MongoClient(env.MONGO_URL);
 
 const getDB = async () => {
-    try {
-        const connection = await client.connect();
-        return connection.db("worddangler");
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const connection = await client.connect();
+    return connection.db("worddangler");
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const closeDB = async () => {
-    try {
-        await client.close();
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await client.close();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 console.log(await getDB());
--- index.mjs
+++ index.mjs
@@ -1,19 +1,21 @@
+import { env } from "node:process";
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
-import {env} from "node:process";
 dotenv.config();
 
 const app = express();
 const port = env.SERVER_PORT;
 
-app.use(cors({
+app.use(
+  cors({
     origin: `http://${env.DOMAIN}:${env.CLIENT_PORT}`,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
+  console.log(`server started on port ${port}`);
 });
